8c3d62a73f5f956e7c5032d0ac04e28ef75f3886
berkin <you@example.com>
2025-10-01 14:37:42 +0300

    +

--- rdtsc.c
+++ rdtsc.c
@@ -21,9 +21,9 @@ int main() {
         printf("Failed to set thread affinity\n");
         return 1;
     }
-
+    uint16_t LOOP_COUNT= 1e6;
     uint64_t start = read_tsc();
-    for (volatile int i = 0; i < 1000000; ++i);
+    for (volatile int i = 0; i < LOOP_COUNT; ++i);
     uint64_t end = read_tsc();
     printf("TSC cycles: %llu\n", end - start);
         uint64_t elapsed_cycles = end - start;
@@ -32,5 +32,10 @@ int main() {
 
     printf("Elapsed cycles: %llu\n", elapsed_cycles);
     printf("Elapsed time: %.3f microseconds\n", elapsed_us);
+    printf("cycle per iteration: %.36f c's\n", (double)elapsed_cycles/LOOP_COUNT);
+    // Time per cycle in nanoseconds
+    double ns_per_cycle = 1e9 / (cpu_ghz * 1e9);  // = 0.357 ns
+    printf("single cycle time: %.36f ns\n", (double)ns_per_cycle);
+
     return 0;
 }
